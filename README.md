a07e5f239c856508f8d97c012469b85b416d29aa
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-08-17 13:19:04 -0700

    Add the numberOfOccurrencesInText function and UI logic
    Co-authored-by: Benjamin Wilson <benjaminw1030@gmail.com>

--- README.md
+++ README.md
@@ -21,12 +21,19 @@ Expected Output: 0;
 
 Describe: numberOfOccurrencesInText()
 
-Test: "It should return 0 occurrences of a word for an empty string"
+Test: "It should return 0 occurrences of a word if text is an empty string"
 Code: 
 const text = "";
 const word = "red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 0;
+
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+wordCounter(word, text);
+Expected Output: 0
 
 Test: "It should return 1 occurrence of a word when the word and the text are the same."
 Code:
@@ -35,7 +42,7 @@ const word = "red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 1;
 
-Test: "It should return 0 occurrences of a word when the owrd and the text are different."
+Test: "It should return 0 occurrences of a word when the word and the text are different."
 Code:
 const text = "red";
 const word = "blue";
--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ function wordCounter(text) {
     return 0;
   }
   let wordCount = 0;
-  const wordArray = text.split(" ");
+  const wordArray = text.trim().split(" ");
   wordArray.forEach(function(element) {
-    if (!Number(element)) {
+    if (!Number(element) && element != "") {
       wordCount++;
     }
   });
@@ -15,15 +15,29 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (text.trim().length === 0 || word.trim().length === 0) {
     return 0;
   }
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element) {
-    if ( element.toLowerCase().includes(word.toLowerCase()) ) {
+    if (element.toLowerCase().includes(word.toLowerCase()) ) {
       wordCount++;
     }
   });
   return wordCount;
 }
+
+// UI Logic
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
